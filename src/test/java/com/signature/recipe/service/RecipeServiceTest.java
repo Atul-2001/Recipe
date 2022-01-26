b2ce57268c76096dfe89751d03ed24b152ff4b7b
Atendra Suryvanshi <suryavanshi.atendra15@gmail.com>
2022-01-26 23:49:22 +0530

    adding mockito example, recipe service test

---
+++ src/test/java/com/signature/recipe/service/RecipeServiceTest.java
@@ -0,0 +1,62 @@
+package com.signature.recipe.service;
+
+import com.signature.recipe.model.Recipe;
+import com.signature.recipe.repository.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+
+class RecipeServiceTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeService(recipeRepository);
+    }
+
+    @Test
+    void getAllRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        List<Recipe> recipes = recipeService.getAllRecipes();
+
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getById() {
+        when(recipeRepository.findById(anyLong()))
+                .thenReturn(Optional.of(Recipe.builder().id(1L).build()));
+
+        Recipe recipeReturned = recipeService.getById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+}
